4f98dc149919e480cb68327f9922dba8c1817ea5
191130620@qq.com <h191130620>
2018-11-05 15:36:40 +0800

    zipkin提交

---
+++ src/views/monitor/zipkin/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <iframe src='http://admin.tgcloud.net:8011/zipkin/' id='show' width='100%' height="660px" frameborder=0 name='showAdmin' ></iframe>
+</template>
+
+<script>
+export default {
+  name: '服务链路'
+}
+</script>
