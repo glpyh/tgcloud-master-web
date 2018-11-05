6204bd32ca8838e01ae07c593eb364f61b5036af
191130620@qq.com <h191130620>
2018-11-05 21:37:39 +0800

    提交代码

--- src/views/monitor/hystrix/index.vue
+++ src/views/monitor/hystrix/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-container">
-    <span>Hystrix Stream: http://zipkin.tgcloud.net:7979/actuator/hystrix.stream</span>
+    <span>Hystrix Stream: http://gateway.tgcloud.net:7979/actuator/hystrix.stream</span>
     <iframe src='http://hystrix.tgcloud.net:8010/hystrix' id='show' width='100%' height="600px" frameborder=0 name='showHystrix' ></iframe>
   </div>
 </template>
